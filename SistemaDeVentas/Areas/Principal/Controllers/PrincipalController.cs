4ae452d2286afdc6af4689f9cdf8d014cd153076
emilio barrera <barrera_emilio@yahoo.es>
2018-10-31 17:53:12 +0000

    all about to close session is alreaady

--- SistemaDeVentas/Areas/Principal/Controllers/PrincipalController.cs
+++ SistemaDeVentas/Areas/Principal/Controllers/PrincipalController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SistemaDeVentas.Areas.Principal.Controllers
-{  [Area("Principal")]
+{
+    [Authorize]
+    [Area("Principal")]
     public class PrincipalController : Controller
     {
         public IActionResult Index()
---
+++ SistemaDeVentas/Areas/Usuarios/Controllers/UsuariosController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SistemaDeVentas.Controllers;
+
+namespace SistemaDeVentas.Areas.Usuarios.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        #region Attributtes
+        private readonly SignInManager<IdentityUser> signInManager;
+        #endregion
+
+        #region Contructors
+        public UsuariosController(SignInManager<IdentityUser> signInManager)
+        {
+            this.signInManager = signInManager;   
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public async Task<IActionResult> SessionClose()
+        {
+            //aqui elimino todos los usurios que iniciaron session:
+            await this.signInManager.SignOutAsync();
+
+            return RedirectToAction(nameof(HomeController.Index),"Home");
+        }
+    }
+}
